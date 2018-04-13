ab1eb574c403775ef0cde3d2ed8577560b5e822d
gj <gj@xkeshi.com>
2018-04-14 01:54:11 +0800

    feat: 代码优化

--- bin/index.js
+++ bin/index.js
@@ -1,52 +1,40 @@
 #!/usr/bin/env node
 const { spawnSync } = require('child_process')
 
-function runCommand(command, options = []) {
-  spawnSync(command, options, {
-    stdio: 'inherit',
-    shell: true,
-  })
-  process.exit(0)
+const commands = {
+  start: 'npm run dll && webpack-dev-server --config webpack/webpack.dev.js',
+  fix: 'eslint src --ext .js,.vue --fix',
+  dll: 'webpack --config webpack/webpack.dll.js',
+  build: 'npm run dll && webpack --config webpack/webpack.prod.js',
+  help: 'help',
+}
+
+const runCommand = (command, options = []) => {
+  if (command === 'help') {
+    console.log('Commands List')
+    console.log('--------------------------------------------------------------------------------------------')
+    Object.keys(commands).map(key => console.log(key.padEnd(6), ':', commands[key]))
+    console.log('--------------------------------------------------------------------------------------------')
+
+    process.exit(0)
+  }
+  if (command) {
+    spawnSync(command, options, {
+      stdio: 'inherit',
+      shell: true,
+    })
+    process.exit(0)
+  } else {
+    console.log('Bad command parameters！Run help to get help')
+    process.exitCode = 1
+  }
 }
 
 // 获取命令的参数
 const args = process.argv.slice(2)
 
-const help = `
-Commands List
----------------
-start :webpack --config webpack/webpack.dll.js && webpack-dev-server --config webpack/webpack.dev.js
-dll   :webpack --config webpack/webpack.dll.js
-fix   :eslint src --ext .js,.vue --fix
-build :webpack --config webpack/webpack.dll.js && webpack-dev-server --config webpack/webpack.prod.js
-help  :show help
----------------
-`
-
 if (args.length) {
-  const arg = args[0]
-  switch (arg) {
-    case '-h':
-    case 'help':
-      console.error(help)
-      process.exitCode = 1
-      break
-    case 'start':
-      runCommand('webpack --config webpack/webpack.dll.js && webpack-dev-server --config webpack/webpack.dev.js')
-      break
-    case 'dll':
-      runCommand('webpack --config webpack/webpack.dll.js')
-      break
-    case 'fix':
-      runCommand('eslint src --ext .js,.vue --fix')
-      break
-    case 'build':
-      runCommand('webpack --config webpack/webpack.dll.js && webpack --config webpack/webpack.prod.js')
-      break
-    default:
-      console.log('Bad command parameters！Run help to get help')
-      process.exitCode = 1
-  }
+  runCommand(commands[args[0]])
 } else {
   console.log('Missing command parameters！Run help to get help')
   process.exitCode = 1
